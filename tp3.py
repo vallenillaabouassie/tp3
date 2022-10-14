10970c1073034f81ff5fa3233a74686b5ef4c5f5
vallenillaabouassie <vallenillaabouassie@educ.college-montreal.qc.ca>
2022-10-14 10:43:12 -0400

    commit initial

---
+++ tp3.py
@@ -0,0 +1,24 @@
+import random
+hp = 20
+print("Vous êtes attrapés dans le dongeon! pour fuire, il vous faut tuer 3 monstres pour retrouver la sortie")
+
+def hud():
+    choice = int(input("Que voulez - vous faire ? 1 - Combattre cet adversaire 2 - Contourner cet adversaire et aller ouvrir une autre porte 3 - Afficher les regles du jeu 4 - Quitter la partie : "))
+    if choice == 1:
+        print("1")
+
+    elif choice == 2:
+        print("2")
+
+    elif choice == 3:
+        print("3")
+
+    elif choice == 4:
+        print("4")
+
+
+
+
+
+hud()
+
